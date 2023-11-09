1e9d7b36189e40a9d8c15225388eef0affe7bd29
pratsy91 <pratapsinghchouhan412@gmail.com>
2023-11-10 00:22:07 +0530

    Initialize

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  const getData = async () => {
+    const response = await axios.post(
+      "https://hoblist.com/api/movieList?category=movies&language=kannada&genre=all&sort=voting",
+      {
+        category: "movies",
+        language: "kannada",
+        genre: "all",
+        sort: "voting",
+      }
+    );
+
+    setMovies(response.data.result);
+  };
+  if (movies.length > 0) {
+    console.log(movies);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hell</h1>
     </div>
   );
 }
